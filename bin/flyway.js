43a99117744fa1f5a9430316f65fd5cece413e56
Neil Bhadsavle <neil.bhadsavle@gtri.gatech.edu>
2024-05-28 11:03:38 -0400

    Revert "Updated using sameer-hashflow's changes to allow use of javascript to call flyway functions"
    This reverts commit caa0e91fe3984e7980e3b00f9e65754a38d5adf3.

--- bin/flyway.js
+++ bin/flyway.js
@@ -3,11 +3,12 @@
 'use strict';
 
 const program = require('commander');
-const pkg = require('../package.json');
-const exeCommand = require('../lib/exec').exeCommand;
 const path = require('path');
-
-
+const spawn = require('child_process').spawn;
+const fs = require('fs');
+const os = require('os');
+const pkg = require('../package.json');
+const download = require('../lib/download');
 
 process.title = 'flyway';
 program
@@ -16,7 +17,6 @@ program
     .on('--help', function() {
         console.log('  See Flyway\'s configuration options at https://flywaydb.org/documentation/commandline/');
     });
-
 
 makeCommand('migrate', 'Migrates the schema to the latest version. Flyway will create the metadata table automatically if it doesn\'t exist.');
 makeCommand('clean', 'Drops all objects (tables, views, procedures, triggers, ...) in the configured schemas. The schemas are cleaned in the order specified by the schemas property.');
@@ -40,10 +40,25 @@ function makeCommand(name, desc) {
     program
         .command(name)
         .description(desc)
-        .action(cliExec);
+        .action(exeCommand);
 }
 
-function cliExec(cmd) {
+function configFlywayArgs(config) {
+    const flywayArgs = config.flywayArgs || {};
+    const flywayArgsKeys = Object.keys(flywayArgs);
+
+    return flywayArgsKeys.map(function(key) {
+        return `-${key}=${flywayArgs[key]}`;
+    });
+}
+
+function binIsFile(path) {
+    const stats = fs.statSync(path);
+
+    return !!stats && stats.isFile();
+}
+
+function exeCommand(cmd) {
     if(!program.configfile) {
         throw new Error('Config file option is required');
     }
@@ -53,5 +68,38 @@ function cliExec(cmd) {
     if (typeof config === 'function') {
         config = config();
     }
-    return exeCommand(config, cmd).then(code => process.exit(code));
+
+    download.ensureArtifacts(config, function(err, flywayBin) {
+        const workingDir = process.cwd();
+
+        if(err) {
+            throw new Error(err);
+        }
+
+        // Ensure that the flywayBin is a file, helps with security risk of having
+        // shell true in the spawn call below
+        if (!binIsFile(flywayBin)) {
+            throw new Error('Flyway bin was not found at "' + flywayBin + '"');
+        }
+
+        const args = configFlywayArgs(config)
+            .concat([cmd._name]);
+
+        // Fix problem with spaces on windows OS
+        // https://github.com/nodejs/node/issues/7367
+        const isWindowsAndHasSpace = !!(flywayBin.match(/\s/) && os.platform() === 'win32');
+        const safeSpawnBin = isWindowsAndHasSpace ? '"' + flywayBin + '"' : flywayBin;
+
+        const child = spawn(safeSpawnBin, args, {
+            env: Object.assign({}, process.env, config.env),
+            cwd: workingDir,
+            stdio: 'inherit',
+            windowsVerbatimArguments: true, // Super Weird, https://github.com/nodejs/node/issues/5060
+            shell: true,
+        });
+
+        child.on('close', code => {
+            process.exit(code);
+        });
+    });
 }
